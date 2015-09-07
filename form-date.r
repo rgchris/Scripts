7f5bc30209546f7686cce764bb9744f836e138ec
Christopher Ross-Gill <chris.rg@ross-gill.com>
2015-09-06 21:44:23 -0400

    SCRIPT: Form Date v1.1.0—Redesign using REWORD.

--- form-date.r
+++ form-date.r
@@ -1,40 +1,20 @@
-REBOL [
-	Title: "Form Date"
+Rebol [
+	Title: "Date Formatter for Rebol 3"
 	Author: "Christopher Ross-Gill"
-	Date: 12-Jun-2013
-	Version: 1.0.0
+	Date: 6-Sep-2015
+	Home: http://scripts.rebol.info/scripts/form-date,docs
 	File: %form-date.r
-	Type: module
-	Name: form-date
-	Exports: [form-date form-time]
+	Version: 1.1.0
 	Purpose: {Return formatted date string using strftime style format specifiers}
 	Rights: http://opensource.org/licenses/Apache-2.0
+	Type: 'module
+	Name: 'rgchris.form-date
+	Exports: [form-date form-time]
+	History: [
+		06-Sep-2015 1.1.0 "Change to use REWORD; Deprecate /GMT"
+		12-Jun-2013 1.0.0 "Ported from Rebol 2"
+	]
 	Comment: {Extracted from the QuarterMaster web framework}
-	Usage: http://www.rebol.org/documentation.r?script=form-date.r
-]
-
-get-class: func [classes [block!] item][
-	all [
-		classes: find classes item
-		classes: find/reverse classes type? pick head classes 1
-		first classes
-	]
-]
-
-interpolate: func [body [string!] escapes [any-block!] /local out][
-	body: out: copy body
-
-	parse/all body [
-		any [
-			to #"%" body: (
-				body: change/part body reduce any [
-					select/case escapes body/2 body/2
-				] 2
-			) :body
-		]
-	]
-
-	out
 ]
 
 pad: func [text length [integer!] /with padding [char!]][
@@ -52,12 +32,9 @@ pad-zone: func [time /flat][
 	]
 ]
 
-pad-precise: func [second /local point] [
-	second: form second
-	unless point: find second "." [insert point: tail second "."]
-	second: pad copy/part second point 2
-	point: head change copy ".000000" point
-	join second point
+pad-precise: func [seconds [number!] /local out][
+	seconds: form make time! seconds
+	head change copy "00.000000" find/last/tail form seconds ":"
 ]
 
 to-epoch-time: func [date [date!]][
@@ -98,13 +75,17 @@ date-codes: [
 	#"B" [pick system/locale/months date/month]
 	#"C" [to integer! date/year / 100]
 	#"d" [pad date/day 2]
-	#"D" [date/year #"-" pad date/month 2 #"-" pad date/day 2]
+	#"D" [
+		rejoin [
+			date/year #"-" pad date/month 2 #"-" pad date/day 2
+		]
+	]
 	#"e" [date/day]
 	#"g" [pad to integer! (second to-iso-week date) // 100 2]
 	#"G" [to integer! second to-iso-week date]
 	#"h" [time/hour + 11 // 12 + 1]
 	#"H" [pad time/hour 2]
-	#"i" [any [get-class ["st" 1 21 31 "nd" 2 22 "rd" 3 23] date/day "th"]]
+	#"i" [switch/default date/day [1 21 31 ["st"] 2 22 ["nd"] 3 23 ["rd"]]["th"]]
 	#"I" [pad time/hour + 11 // 12 + 1 2]
 	#"j" [pad date/julian 3]
 	#"J" [date/julian]
@@ -116,7 +97,11 @@ date-codes: [
 	#"s" [to-epoch-time date]
 	#"S" [pad to integer! time/second 2]
 	#"t" [#"^-"]
-	#"T" [pad time/hour 2 #":" pad time/minute 2 #":" pad to integer! time/second 2]
+	#"T" [
+		rejoin [
+			pad time/hour 2 #":" pad time/minute 2 #":" pad to integer! time/second 2
+		]
+	]
 	#"u" [date/weekday]
 	#"U" [pad to integer! date/julian + 6 - (date/weekday // 7) / 7 2]
 	#"V" [pad to integer! first to-iso-week date 2]
@@ -129,28 +114,43 @@ date-codes: [
 	#"Z" [pad-zone zone]
 
 	#"c" [
-		date/year #"-" pad date/month 2 "-" pad date/day 2 "T"
-		pad time/hour 2 #":" pad time/minute 2 #":" pad to integer! time/second 2 
-		either gmt ["Z"][pad-zone zone]
+		rejoin [
+			date/year #"-" pad date/month 2 "-" pad date/day 2 "T"
+			pad time/hour 2 #":" pad time/minute 2 #":" pad to integer! time/second 2 
+			either utc ["Z"][pad-zone zone]
+		]
 	]
 ]
 
-form-date: func [date [date!] format [any-string!] /gmt /local time zone nyd][
+form-date: func [
+	"Renders a date to a given format"
+	date [date!] "Date to gormat"
+	format [any-string!] "Format (string largely compatible with strftime)"
+	/utc "Align time with UTC"
+	/gmt "To be deprecated"
+	/local time zone
+][
+	if all [gmt not system/options/quiet][
+		print "/GMT is to be deprecated, use /UTC"
+	]
+
+	utc: any [utc gmt]
+
 	either date/time [
 		if date/zone [date/time: date/time - date/zone]
-		date/zone: either gmt [0:00][date/zone]
+		date/zone: either utc [0:00][date/zone]
 		date/time: date/time + date/zone
 	][
 		date/time: 0:00
-		date/zone: either gmt [0:00][now/zone]
+		date/zone: either utc [0:00][now/zone]
 	]
 
 	time: date/time
 	zone: date/zone
-	interpolate format bind date-codes 'date
+	reword/case/escape format bind date-codes 'date #"%"
 ]
 
 form-time: func [time [time!] format [any-string!] /local date zone][
 	date: now/date zone: 0:00
-	interpolate format bind date-codes 'time
+	reword/case/escape format bind date-codes 'time #"%"
 ]
