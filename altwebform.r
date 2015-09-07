4d2ae588e20c58333ccf39b83a2d5318cb465a64
Christopher Ross-Gill <chris.rg@ross-gill.com>
2015-09-06 21:41:58 -0400

    SCRIPT: AltWebForm v0.10.0—Add support for encoding 'Ruby style' structured keys.

--- altwebform.r
+++ altwebform.r
@@ -1,15 +1,22 @@
-REBOL [
-	Title: "REBOL Web Form Encoder/Decoder"
+Rebol [
+	Title: "Web Form Encoder/Decoder for Rebol 3"
 	Author: "Christopher Ross-Gill"
 	Date: 6-Jul-2013
-	Version: 0.9.5
-	Purpose: "Convert a Rebol block to a URL-Encoded Web Form string"
-	Name: altwebform
 	Home: http://www.ross-gill.com/page/Web_Forms_and_REBOL
 	File: %altwebform.r
+	Version: 0.10.0
+	Purpose: "Convert a Rebol block to a URL-Encoded Web Form string"
+	Rights: http://opensource.org/licenses/Apache-2.0
 	Type: 'module
-	History: [18-Nov-2009 "Original Version"]
+	Name: 'rgchris.altwebform
 	Exports: [url-decode url-encode load-webform to-webform]
+	History: [
+		06-Sep-2015 0.10.0 "Add Ruby-style paths to encoding"
+		06-Jul-2013  0.9.5 "Fix encoding/decoding of _ character"
+		01-Mar-2013  0.9.4 "Detach URL-DECODE and URL-ENCODE"
+		27-Feb-2013  0.9.2 "Correct encoding of UTF-8 values"
+		18-Nov-2009  0.1.0 "Original Version"
+	]
 	Example: [
 		"a=3&aa.a=1&b.c=1&b.c=2"
 		[a "3" aa [a "1"] b [c ["1" "2"]]]
@@ -22,7 +29,7 @@ url-decode: use [as-is hex space][
 
 	func [text [any-string!] /wiki][
 		space: either wiki [#"_"][#"+"]
-		either parse/all text: to binary! text [
+		either parse text: to binary! text [
 			copy text any [
 				  some as-is | remove space insert " "
 				| [#"_" | #"+" | #"." | #","]
@@ -41,7 +48,7 @@ url-encode: use [as-is space percent-encode][
 
 	func [text [any-string!] /wiki][
 		space: either wiki [#"_"][#"+"]
-		either parse/all text: to binary! text [
+		either parse text: to binary! text [
 			copy text any [
 				  text: some as-is | end | change " " space
 				| [#"_" | #"."] (either wiki [percent-encode text][text])
@@ -51,14 +58,16 @@ url-encode: use [as-is space percent-encode][
 	]
 ]
 
-load-webform: use [result path string pair as-path][
+load-webform: use [result path string pair as-path term][
 	result: copy []
 
-	as-path: func [name [string!]][to path! to block! replace/all name #"." #" "]
+	as-path: func [name [string!]][
+		to path! to block! replace/all name #"." #" "
+	]
 
 	path: use [aa an wd][
-		aa: charset [#"A" - #"Z" #"a" - #"z"]
-		an: charset [#"-" #"0" - #"9" #"A" - #"Z" #"-" #"a" - #"z"]
+		aa: charset [#"a" - #"z" #"A" - #"Z" #"_"]
+		an: charset [#"-" #"0" - #"9" #"a" - #"z" #"A" - #"Z" #"_"]
 		wd: [aa 0 40 an] ; one alpha, any alpha/numeric/dash/underscore
 		[wd 0 6 [#"." wd]]
 	]
@@ -69,10 +78,13 @@ load-webform: use [result path string pair as-path][
 		[any [ch | #"+" | #"%" 2 hx]] ; any [unreserved | percent-encoded]
 	]
 
+	term: [#"&" | end]
+
 	pair: use [name value tree][
 		[
-			copy name path #"=" copy value string [#"&" | end]
-			(
+			copy name path [
+				#"=" copy value string term | term (value: none)
+			] (
 				tree: :result
 				name: as-path name
 				value: url-decode value
@@ -104,19 +116,24 @@ load-webform: use [result path string pair as-path][
 		webform: any [webform ""]
 		result: copy []
 
-		either parse/all webform [opt #"&" any pair][result][
+		either parse webform [opt [#"&" | #"?"] any pair][result][
 			make error! "Not a URL Encoded Web Form"
 		]
 	]
 ]
 
 to-webform: use [
-	webform form-key emit
+	webform form-key emit ruby-style?
 	here path reference value block array object
 ][
 	path: []
 	form-key: does [
-		remove head foreach key path [insert "" reduce ["." key]]
+		rejoin collect [
+			keep first path
+			foreach key next path [
+				keep reduce either ruby-style? [["[" key "]"]][["." key]]
+			]
+		]
 	]
 
 	emit: func [data][
@@ -143,8 +160,10 @@ to-webform: use [
 
 	object: [
 		and opt reference any [
-			here: [word! | set-word!] (insert path to-word here/1)
-			[value | block] (remove path)
+			here: [word! | set-word!] (
+				append path url-encode to string! to word! here/1
+			)
+			[value | block] (remove back tail path)
 		] end
 	]
 
@@ -152,16 +171,18 @@ to-webform: use [
 		here: and [
 			  any-block! (change/only here copy here/1)
 			| object! (change/only here body-of here/1)
-		] into [object | mk: array]
+		] into [object | array]
 	]
 
 	func [
 		"Serializes block data as URL-Encoded Web Form string"
-		data [block! object!] /prefix
+		data [block! object!] /prefix /ruby-style
 	][
 		clear path
 		webform: copy ""
 		data: either object? data [body-of data][copy data]
+		ruby-style?: :ruby-style
+
 		if parse copy data object [
 			either all [
 				prefix not tail? next webform
